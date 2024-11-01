44273ea7bef7ae21c41b840610abee0a73306edd
Roberto Russo <robertorusso@MacBook-Pro-di-The.local>
2024-11-01 21:29:17 +0100

    list all products x client

--- public/script.js
+++ public/script.js
@@ -135,6 +135,7 @@ searchButton.addEventListener("click", async () => {
                 <li class="purchase-item">
                   <p><strong>Prodotto:</strong> ${acquisto.product}</p>
                   <p><strong>Prezzo:</strong> €${acquisto.price.toFixed(2)}</p>
+                  <p><strong>Data d'acquisto:</strong> ${acquisto.date}</p>
                   <p><strong>Note:</strong> ${
                     acquisto.notes || "Nessuna nota"
                   }</p>
